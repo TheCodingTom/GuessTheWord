4e90522ce4c6afe9c011f07c2c1ff28251c83716
TheCodingTom <dastolto.t@gmail.com>
2025-05-14 16:20:20 +0200

    tracking current guess works

---
+++ src/hooks/useWordle.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+const useWordle = (solution) => {
+  const [turn, setTurn] = useState(0);
+  const [currentGuess, setCurrentGuess] = useState("");
+  const [guesses, setGuesses] = useState([]); // each guess is an array
+  const [history, setHistory] = useState([]); // each guess is a string - to avoid duplicates
+  const [isCorrect, setIsCorrect] = useState(false);
+
+  // format a guess into an array of letter objects --> [{key: "a", color: "yellow"}]
+  const formatGuess = () => {};
+
+  // add a new guess to the guesses state (history of guesses)
+  // update the isCorrect state if the guess is correct
+  // add one to the turn state
+  const addNewGuess = () => {};
+
+  // handle keyip event & track current guess - if user presses enter, add the new guess
+  const handleKeyup = ({ key }) => {
+    if (key === "Backspace") {
+      setCurrentGuess((prev) => {
+        return prev.slice(0, -1);
+      });
+      return;
+    }
+
+    if (/^[A-Za-z]$/.test(key)) {
+      if (currentGuess.length < 5) {
+        setCurrentGuess((prev) => {
+          return prev + key;
+        });
+      }
+    }
+  };
+
+  return { turn, currentGuess, guesses, isCorrect, handleKeyup };
+};
+
+export default useWordle;
